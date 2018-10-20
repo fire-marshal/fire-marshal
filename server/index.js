5def21c619cedfa17d6db5acfe66a6fb33ceaac9
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-10-20 22:27:52 +0200

    boostrap koa server

--- server/index.js
+++ server/index.js
@@ -1,30 +1,10 @@
-const googleTrends = require('google-trends-api');
+const {server} = require('./lib');
 
-// googleTrends.interestByRegion({
-//   keyword: 'fire',
-//   // startTime: new Date(2018, 9, 1),
-//   endTime: new Date(Date.now()),
-//   geo: 'Ukraine',
-//   resolution: 'DMA',
-// })
-//   .then(function (results) {
-//     console.log('These results are awesome', results);
-//   })
-//   .catch(function (err) {
-//     console.error('Oh no there was an error', err);
-//   });
 
-googleTrends.interestByRegion({
-  keyword: 'огонь',
-  geo: 'UA',
-  // startTime: new Date('2017-02-01'),
-  endTime: new Date(Date.now()),
-  resolution: 'CITY'
-})
-  .then((res) => {
-    console.log(typeof res);
-    console.log(JSON.stringify(JSON.parse(res), null, 2));
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const config = {
+  server: {
+    port: 8080,
+  },
+};
+
+server.bootstrap(config);
---
+++ server/lib/server.js
@@ -0,0 +1,23 @@
+const Koa = require('koa');
+
+/**
+ * Run server
+ *
+ * @param config
+ * @returns {module.Application|*}
+ */
+function bootstrap (config) {
+  const app = new Koa();
+
+  app.use(async ctx => {
+    ctx.body = 'Hello World';
+  });
+
+  app.listen(config.server.port);
+
+  return app;
+}
+
+module.exports = {
+  bootstrap,
+};
