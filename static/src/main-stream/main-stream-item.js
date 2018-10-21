879f1ebc464fb25b2bafe5ef68e5139dd4e68492
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-10-21 21:40:47 +0200

    style alerts stream

--- static/src/main-stream/main-stream-item.js
+++ static/src/main-stream/main-stream-item.js
@@ -1,15 +1,19 @@
+import './main-stream-item.scss'
+
 import React from 'react'
 
 const MainStreamItem = ({ item }) => (
-  <div>
-    <h3>{item.title}</h3>
-    <div>
-      <img src={item.img.src} alt={item.title} />
+  <div className='card alert-card'>
+    <img className='card-img-top' src={item.img.src} alt={item.title} />
+    <div className='card-body'>
+      <h5 className='card-title'>{item.title}</h5>
+      <div className='card-text'>
+        <div>{item.when.exactlyAt.toString()}</div>
+        <div>tags: {item.tags}</div>
+        <div>power: {item.power}</div>
+        <div>@{item.author}</div>
+      </div>
     </div>
-    <div>{item.when.exactlyAt.toString()}</div>
-    <div>tags: {item.tags}</div>
-    <div>power: {item.power}</div>
-    <div>@{item.author}</div>
   </div>
 )
 
--- static/src/main-stream/main-stream.js
+++ static/src/main-stream/main-stream.js
@@ -1,10 +1,12 @@
+import './main-stream.scss'
+
 import React from 'react'
 import { connect } from 'react-redux'
 
 import MainStreamItem from './main-stream-item'
 
 const MainStream = ({ items }) => (
-  <div className='container'>
+  <div className='container main-stream-container'>
     {items.map(item => <MainStreamItem key={item.id} item={item} />)}
   </div>
 )
