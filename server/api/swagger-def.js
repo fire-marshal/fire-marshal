761cd939771e73e90f2604f475fc1795b96e9736
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-10-29 13:14:49 +0100

    build open api 3.0 and show it by swagger-ui

---
+++ server/api/swagger-def.js
@@ -0,0 +1,9 @@
+module.exports = {
+  info: {
+    title: 'Fire Marshal', // Title (required)
+    version: '0.1.0', // Version (required)
+    description: 'Fire Marshal APP API' // Description (optional)
+  }
+  // host, // Host (optional)
+  // basePath: '/', // Base path (optional)
+}
