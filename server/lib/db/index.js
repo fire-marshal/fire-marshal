5d46df85578aa90866a67ec27e95978870da67d7
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-11-04 01:57:39 +0100

    use _.memoize to preserve db between requests

--- server/lib/db/index.js
+++ server/lib/db/index.js
@@ -1,23 +1,20 @@
+const _ = require('lodash')
 const MongoClient = require('mongodb').MongoClient
 
 class DB {
   constructor (config) {
     this.config = config
     this.db = null
+    this.getDB = _.memoize(this.getDB)
   }
 
   async getDB () {
-    if (this.db) {
-      return this.db
-    }
-
     const client = new MongoClient(this.config.mongo.url, {
       useNewUrlParser: true,
       authSource: 'admin'
     })
     await client.connect()
-    this.db = client.db(this.config.mongo.db)
-    return this.db
+    return client.db(this.config.mongo.db)
   }
 
   async getFireCollection () {
