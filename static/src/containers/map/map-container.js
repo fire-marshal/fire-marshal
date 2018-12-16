a2e53b32ac9ba9e411d61167de84adfca8e33455
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-12-16 17:20:28 +0100

    add map container

---
+++ static/src/containers/map/map-container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MapContainer = () => {
+  return (
+    <div><h1>Map Container</h1></div>
+  )
+}
+
+MapContainer.displayName = 'MapContainer'
+
+export default MapContainer
