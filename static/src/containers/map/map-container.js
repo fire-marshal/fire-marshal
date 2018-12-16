3468640b3915248c6be4cca27aaf06760164cc91
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-12-16 21:39:48 +0100

    install leaflet and put it on screen

--- static/src/containers/map/map-container.js
+++ static/src/containers/map/map-container.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import './map-container.scss'
+
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+import React, { useEffect, useRef } from 'react'
 
 const MapContainer = () => {
+  const mapRef = useRef()
+  useEffect(() => {
+    console.log('create map!')
+    const map = L.map(mapRef.current).setView([51.505, -0.09], 13)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map)
+  }, [mapRef])
+
   return (
-    <div><h1>Map Container</h1></div>
+    <div ref={mapRef} className='map-container'>
+      <h1>Map Container!</h1>
+    </div>
   )
 }
 
