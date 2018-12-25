c0deef823014f6b940ed0845cd4795f446007b57
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-12-25 01:48:42 +0100

    fix connected-react-router@6 integration

--- static/src/reducers/index.js
+++ static/src/reducers/index.js
@@ -1,9 +1,11 @@
+import { connectRouter } from 'connected-react-router/immutable'
 import { combineReducers } from 'redux-immutable'
 
 import entities from './entities'
 import ui from './ui'
 
-export default combineReducers({
+export default (history) => combineReducers({
   entities,
+  router: connectRouter(history),
   ui
 })
