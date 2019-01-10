079d7d59c8fa55c2332b81f3cbe45918a1f55189
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2019-01-11 00:14:15 +0100

    copy test config for dev env

--- static/config/webpack.config-dev.js
+++ static/config/webpack.config-dev.js
@@ -1,23 +1,10 @@
 const nodeExternals = require('webpack-node-externals')
-const path = require('path')
-
-const isCoverage = process.env.NODE_ENV === 'coverage'
 
 module.exports = {
   mode: 'development',
   module: {
-    rules: [].concat(
-      isCoverage ? {
-        test: /\.(js|ts)/,
-        include: path.resolve('src'), // instrument only testing sources with Istanbul, after ts-loader runs
-        loader: 'istanbul-instrumenter-loader',
-        query: {
-          esModules: true
-        }
-      } : [],
-      // we those rules build takes longer
-      // but maybe because we have small styles yet
-      // would compare for bigger style files
+    rules: [
+      // TODO: setup styles
       { test: /\.scss$/, loader: 'null-loader' },
       { test: /\.css$/, loader: 'null-loader' }, {
         test: /\.jsx?$/,
@@ -28,9 +15,9 @@ module.exports = {
         test: /\.(jpg|gif|png|woff|woff2|eot|ttf|svg)$/,
         loader: 'null-loader'
       }
-    )
+    ]
   },
   target: 'node',
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   devtool: 'inline-cheap-module-source-map'
-}
+};
